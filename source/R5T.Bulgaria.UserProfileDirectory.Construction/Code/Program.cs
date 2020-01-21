6acc57b5416de389d3f146c15754de0ed711e570
MinexAutomation <minex.automation@gmail.com>
2020-01-20 23:29:35 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Bulgaria.UserProfileDirectory.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Bulgaria.UserProfileDirectory.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
