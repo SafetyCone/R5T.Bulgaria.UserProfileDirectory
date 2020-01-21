7321dc005295d80037bdd88685209aae9ac8a9c4
MinexAutomation <minex.automation@gmail.com>
2020-01-20 23:39:59 -0800

    Added IServiceCollection extensions.

--- source/R5T.Bulgaria.UserProfileDirectory/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Bulgaria.UserProfileDirectory
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Bulgaria.UserProfileDirectory/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Lombardy.Standard;
+using R5T.Peristeria.Standard;
+using R5T.Visigothia.Standard;
+
+using R5T.Bulgaria.Visigothia;
+
+
+namespace R5T.Bulgaria.UserProfileDirectory
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDropboxDirectoryPathProvider(this IServiceCollection services)
+        {
+            services
+                .AddSingleton<IDropboxDirectoryPathProvider, UserProfileDropboxDirectoryPathProvider>()
+                .AddDropboxDirectoryNameConvention()
+                .AddUserProfileDirectoryPathProvider()
+                .AddStringlyTypedPathOperator()
+                ;
+
+            return services;
+        }
+    }
+}
