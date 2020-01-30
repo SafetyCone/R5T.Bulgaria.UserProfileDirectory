a07a88414163da5c943c42358f4d318129029d4e
MinexAutomation <minex.automation@gmail.com>
2020-01-30 14:15:15 -0800

    Updated IServiceCollection extensions to use ServiceAction<T> functionality.

--- source/R5T.Bulgaria.UserProfileDirectory/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.Bulgaria.UserProfileDirectory/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.DependencyInjection;
 
+using R5T.Dacia;
 using R5T.Lombardy.Standard;
 using R5T.Peristeria.Standard;
 using R5T.Visigothia.Standard;
@@ -13,16 +14,26 @@ namespace R5T.Bulgaria.UserProfileDirectory
 {
     public static class IServiceCollectionExtensions
     {
+        /// <summary>
+        /// Adds the <see cref="IDropboxDirectoryPathProvider"/> service.
+        /// </summary>
         public static IServiceCollection AddDropboxDirectoryPathProvider(this IServiceCollection services)
         {
-            services
-                .AddSingleton<IDropboxDirectoryPathProvider, UserProfileDropboxDirectoryPathProvider>()
-                .AddDropboxDirectoryNameConvention()
-                .AddUserProfileDirectoryPathProvider()
-                .AddStringlyTypedPathOperator()
-                ;
+            services.AddUserProfileDropboxDirectoryPathProvider(
+                services.AddUserProfileDirectoryPathProviderAction(),
+                services.AddDropboxDirectoryNameConventionAction(),
+                services.AddStringlyTypedPathOperatorAction());
 
             return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IDropboxDirectoryPathProvider"/> service.
+        /// </summary>
+        public static ServiceAction<IDropboxDirectoryPathProvider> AddDropboxDirectoryPathProviderAction(this IServiceCollection services)
+        {
+            var serviceAction = new ServiceAction<IDropboxDirectoryPathProvider>(() => services.AddDropboxDirectoryPathProvider());
+            return serviceAction;
         }
     }
 }
